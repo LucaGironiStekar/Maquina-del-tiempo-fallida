68b11c72cc01cacca4024a80abd6400203958c55
Luca Gironi Stekar <lucagironistekar@outlook.com>
2022-07-01 06:59:54 -0300

    Add project files.

---
+++ DiaSiguiente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ElDíaDespuésDeMañana
+{
+    public partial class DiaSiguiente : Form
+    {
+        public DiaSiguiente()
+        {
+            InitializeComponent();
+        }
+
+        private override void btViajar_Click(object sender, EventArgs e)
+        {
+            
+
+
+        }
+    }
+}
